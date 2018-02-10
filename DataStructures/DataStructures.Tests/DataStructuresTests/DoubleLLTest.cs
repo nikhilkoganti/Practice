2842061c692e27859e8b298fc07fab718f72fedf
NikhilKoganti <kogantinikhil@gmail.com>
2018-02-10 17:05:20 -0500

    Implemented and tested Double linked list.

---
+++ DataStructures/DataStructures.Tests/DataStructuresTests/DoubleLLTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using DataStructures.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Tests.DataStructuresTests
+{
+    [TestClass]
+    public class DoubleLLTest
+    {
+        [TestMethod]
+        public void DoubleLLAdd()
+        {
+            DoubleLL<int> list = new DoubleLL<int>();
+
+            list.AddLast(1);
+
+            Assert.AreEqual(list.head.value, 1);
+            Assert.AreEqual(list.count, 1);
+        }
+
+        [TestMethod]
+        public void DoubleLLRemove()
+        {
+            DoubleLL<int> list = new DoubleLL<int>();
+
+            list.AddLast(145);
+            list.AddLast(168);
+            list.AddLast(193);
+
+            var node = list.head;
+
+            list.Remove(node);
+
+            Assert.AreEqual(list.head.value, 168);
+            Assert.AreEqual(list.head.next.value, 193);
+
+            node = list.head.next;
+            
+            Assert.AreEqual(node.value, 193);
+            Assert.AreEqual(list.count, 2);
+
+            list.Remove(node);
+
+            Assert.AreEqual(list.head.value, 168);
+            Assert.AreEqual(list.count, 1);
+        }
+
+        [TestMethod]
+        public void IteratingTest()
+        {
+            DoubleLL<int> list = new DoubleLL<int>();
+
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+            list.AddLast(4);
+            list.AddLast(5);
+
+            int i = 1;
+            foreach(var node in list)
+            {
+                Assert.AreEqual(node, i);
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public void IteratingReverseTest()
+        {
+            DoubleLL<int> list = new DoubleLL<int>();
+
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+            list.AddLast(4);
+            list.AddLast(5);
+
+            var nodeEnumerator = list.GetEnumeratorLastToFirst();
+
+            int node = nodeEnumerator.Current;
+
+            int j = 5;
+            while(nodeEnumerator.MoveNext())
+            {
+                node = nodeEnumerator.Current;
+                Assert.AreEqual(node, j);
+                j--;
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/DataStructures/DoubleLL.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataStructures.DataStructures
+{
+    public class DoubleLLNode<T>
+    {
+        public T value;
+        public DoubleLLNode<T> previous;
+        public DoubleLLNode<T> next;
+    }
+
+    public class DoubleLL<T> : IEnumerable<T>
+    {
+        public DoubleLLNode<T> head;
+        public DoubleLLNode<T> tail;
+        public int count = 0;
+
+        public void AddLast(T value)
+        {
+            DoubleLLNode<T> node = new DoubleLLNode<T>();
+            node.value = value;
+
+            if (head == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                node.previous = tail;
+                tail.next = node;
+                tail = node;
+            }
+            count++;
+        }
+
+        public void Remove(DoubleLLNode<T> node)
+        {
+            if (count == 0)
+            {
+                throw new Exception("List is empty");
+            }
+            else
+            {
+                var tempNode = head;
+
+                while(tempNode != null)
+                {
+                    if (tempNode == node)
+                    {
+                        if (tempNode.previous != null && tempNode.next != null)
+                        {
+                            tempNode.previous.next = tempNode.next;
+                            tempNode.next.previous = tempNode.previous;
+                        }
+                        else if (tempNode.previous == null && tempNode.next == null)
+                        {
+                            head = tail = null;
+                        }
+                        else if (tempNode.next == null)
+                        {
+                            tail = tempNode.previous;
+                            tempNode.previous.next = null;
+                        }
+                        else if (tempNode.previous == null)
+                        {
+                            head = tempNode.next;
+                            tempNode.next.previous = null;
+                        }
+                        count--;
+                        return;
+                    }
+                    tempNode = tempNode.next;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = head;
+
+            while(node != null)
+            {
+                yield return node.value;
+                node = node.next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumeratorLastToFirst()
+        {
+            var node = tail;
+
+            while (node != null)
+            {
+                yield return node.value;
+                node = node.previous;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
